8c849365c133688ee38d13a4eebf10138179dae0
Abian <jabiands26@gmail.com>
2025-09-25 13:49:11 +0100

    Version 2 del Hola Mundo

--- holamundo.php
+++ holamundo.php
@@ -1,5 +1,6 @@
 <?php
     echo "<html><body>";
     echo "<h1>Hola mundo!</h1>";
+    echo "<h2>Actualizacion</h2>";
     echo "</body></html>";
 ?>
---
+++ holamundo2.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<html><body>";
+    echo "<h1>Hola mundo, este es otro nuevo archivo</h1>";
+    echo "<h2>Actualizando...</h2>";
+    echo "</body></html>";
+?>
